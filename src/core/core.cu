55a9b74e00b5135691ed5768056d3dd6ce61bd4b
Shunyang Li <shunyangli0@gmail.com>
2024-08-08 14:59:19 +1000

    update the core code

---
+++ src/core/core.cu
@@ -0,0 +1,12 @@
+/**
+ * @author Shunyang Li
+ * Contact: sli@cse.unsw.edu.au
+ * @date on 2024/8/8.
+ * @brief the main file of core decomposition
+ */
+
+/**
+* for 1 .. alpha -> block
+ *  for 1 .. beta -> warp
+ * needs O(n*block) memory
+*/
