86ed8269ae162fd40716d43b46b80edd3b06f517
Shunyang Li <shunyangli0@gmail.com>
2024-08-19 14:14:56 +1000

    update gpu core code

--- src/core/core.cu
+++ src/core/core.cu
@@ -4,9 +4,15 @@
  * @date on 2024/8/8.
  * @brief the main file of core decomposition
  */
+#include "core.cuh"
 
 /**
 * for 1 .. alpha -> block
  *  for 1 .. beta -> warp
- * needs O(n*block) memory
+ * needs O(3n * block) memory
 */
+
+
+auto core_decomposition(Graph* g) -> void {
+
+}
