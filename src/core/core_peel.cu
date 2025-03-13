34fbb55b92f1460b3eb41c68c2c84d69800f0ee0
Shunyang Li <shunyangli0@gmail.com>
2025-03-13 18:19:22 +1100

    update the core based peeling

--- src/core/core_peel.cu
+++ src/core/core_peel.cu
@@ -215,13 +215,14 @@ __global__ auto scan_kernel_core(int* d_degree, uint* buf_tails, uint* g_buffers
 
         if (d_core[v] < k) {
             is_valid[v] = false;
-        }
+            uint threshold = v < u_num ? alpha : beta;
 
-        uint threshold = v < u_num ? alpha : beta;
-
-        if (d_degree[v] < threshold) {
-            uint idx = atomicAdd(&bufTail, 1);
-            writeToBuffer(g_buffer, idx, v);
+            if (d_degree[v] < threshold) {
+                uint idx = atomicAdd(&bufTail, 1);
+                writeToBuffer(g_buffer, idx, v);
+            }
+        } else if (d_core[v] >= k) {
+            is_valid[v] = false;
         }
     }
 
@@ -391,154 +392,3 @@ auto g_abcore_peeling_core(Graph& g, int alpha, int beta) -> double {
     return time;
 }
 
-
-
-
-
-__global__ auto scan_kernel_adv(int* d_degree, uint* d_peels, uint* d_peel_idx,
-                                 int alpha, int beta, uint u_num, uint num_vertex) -> void {
-
-    __syncthreads();
-
-    uint g_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    for (uint base = 0; base < num_vertex; base += N_THREADS) {
-        uint v = base + g_idx;
-        if (v >= num_vertex) continue;
-
-        uint threshold = v < u_num ? alpha : beta;
-
-        if (d_degree[v] < threshold) {
-            uint idx = atomicAdd(d_peel_idx, 1);
-            d_peels[idx] = v;
-        }
-    }
-
-}
-
-
-__global__ auto peel_kernel_adv(const uint* d_offset, const uint* d_neighbors, int* d_degree,
-                                 uint u_num, int alpha, int beta, uint* d_peels, uint* d_idx, uint* d_next,
-                                uint* d_next_idx) -> void {
-
-
-    uint warp_id = threadIdx.x / 32;
-    uint lane_id = threadIdx.x % 32;
-    uint global_warp_id = blockIdx.x * (blockDim.x / 32) + warp_id;
-    uint num_warps = gridDim.x * (blockDim.x / 32);
-
-    for (uint i = global_warp_id; i < *d_idx; i += num_warps) {
-        uint v = d_peels[i];
-        uint start = d_offset[v];
-        uint end = d_offset[v + 1];
-
-
-        for (uint j = start + lane_id; j < end; j += 32) {
-            uint u = d_neighbors[j];
-            int threshold = u < u_num ? alpha : beta;
-
-            int deg_u = atomicSub(d_degree + u, 1);
-
-            if ((deg_u - 1) == (threshold - 1)) {
-                uint loc = atomicAdd(d_next_idx, 1);
-                d_next[loc] = u;
-            }
-        }
-
-        // wait for all threads to finish in the same warp
-        __syncwarp();
-    }
-}
-
-/**
- * abcore online peeling algorithm on gpu
- * @param g graph
- * @param alpha alpha value
- * @param beta beta value
- */
-auto g_abcore_peeling_adv(Graph& g, int alpha, int beta) -> double {
-
-    log_info("running (alpha,beta)-core online peeling algorithm on GPU based on kcore");
-
-    auto left_degree_max = std::max_element(g.degrees, g.degrees + g.u_num);
-    auto right_degree_max = std::max_element(g.degrees + g.u_num, g.degrees + g.n - 1);
-
-    // check if the graph is valid
-    if (*left_degree_max < alpha || *right_degree_max < beta) {
-        log_error("max degree: (%d, %d), query (%d, %d) is not valid", *left_degree_max, *right_degree_max, alpha, beta);
-        return 0;
-    }
-
-    uint* d_offset;
-    uint* d_neighbors;
-    int* d_degree;
-    uint* d_peels;
-    uint* d_peel_idx;
-    uint* d_next;
-    uint* d_next_idx;
-
-    CER(cudaMalloc(&d_offset, sizeof(uint) * (g.n + 1)));
-    CER(cudaMalloc(&d_neighbors, sizeof(uint) * g.m * 2));
-    CER(cudaMalloc(&d_degree, sizeof(int) * g.n));
-    CER(cudaMalloc(&d_peels, sizeof(uint) * g.n));
-    CER(cudaMalloc(&d_peel_idx, sizeof(uint)));
-    CER(cudaMalloc(&d_next, sizeof(uint) * g.n));
-    CER(cudaMalloc(&d_next_idx, sizeof(uint)));
-
-    CER(cudaMemcpy((void*) d_offset, (void*) g.offsets, sizeof(uint) * (g.n + 1), cudaMemcpyHostToDevice));
-    CER(cudaMemcpy((void*) d_neighbors, (void*) g.neighbors, sizeof(uint) * g.m * 2, cudaMemcpyHostToDevice));
-    CER(cudaMemcpy((void*) d_degree, (void*) g.degrees, sizeof(uint) * g.n, cudaMemcpyHostToDevice));
-    CER(cudaMemset((void*) d_peel_idx, 0, sizeof(uint)));
-    CER(cudaMemset((void*) d_next_idx, 0, sizeof(uint)));
-
-
-    int cnt = 0;
-    auto timer = new Timer();
-    timer->reset();
-
-    scan_kernel_adv<<<BLK_NUMS, BLK_DIM, sizeof(uint) * BLK_DIM>>>(d_degree, d_peels, d_peel_idx, alpha, beta, g.u_num, g.n);
-    CER(cudaMemcpy(&cnt, d_peel_idx, sizeof(uint), cudaMemcpyDeviceToHost));
-
-    while (cnt != 0) {
-        peel_kernel_adv<<<BLK_NUMS, BLK_DIM>>>(d_offset, d_neighbors, d_degree, g.u_num, alpha, beta, d_peels, d_peel_idx, d_next, d_next_idx);
-        CER(cudaMemcpy(d_peel_idx, d_next_idx, sizeof(uint), cudaMemcpyDeviceToDevice));
-        CER(cudaMemcpy(d_peels, d_next, sizeof(uint) * g.n, cudaMemcpyDeviceToDevice));
-        CER(cudaMemcpy(&cnt, d_peel_idx, sizeof(uint), cudaMemcpyDeviceToHost));
-    }
-    cudaDeviceSynchronize();
-
-    auto time = timer->elapsed();
-    log_info("abcore peeling time on gpu: %f s", time);
-
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        log_trace("CUDA error: %s", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    auto upper_vertices = std::vector<uint>();
-    auto lower_vertices = std::vector<uint>();
-    auto degrees = new int[g.n];
-
-    // get degrees
-    cudaMemcpy((void*) degrees, (void*) d_degree, sizeof(int) * g.n, cudaMemcpyDeviceToHost);
-
-    // get result
-    for (auto i = 0; i < g.u_num; i++)
-        if (degrees[i] >= alpha) upper_vertices.push_back(i);
-    for (auto i = g.u_num; i < g.n; i++)
-        if (degrees[i] >= beta) lower_vertices.push_back(i);
-
-    // free cuda memory
-    cudaFree(d_offset);
-    cudaFree(d_neighbors);
-    cudaFree(d_degree);
-    cudaFree(d_peels);
-    cudaFree(d_peel_idx);
-    cudaFree(d_next);
-    cudaFree(d_next_idx);
-
-    delete timer;
-
-
-    return time;
-}
